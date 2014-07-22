f45e52939994f8c575675e3cd91e524e7eec2344
mjpigg <mjpigg@gmail.com>
2014-07-22 15:54:50 -0700

    First Test commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,16 @@
 ## Put comments here that give an overall description of what your
 ## functions do
+## test comment for first commit
 
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  y<-lapply(1:4,mean)
 }
 
 
 ## Write a short comment describing this function
-
+## another comment
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
 }
